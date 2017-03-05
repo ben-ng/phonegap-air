9b09ccb5cb1da245aba98d9091c14d7b4563261c
Michael Dautermann <dautermann-github@spamarrest.com>
2017-03-05 06:13:53 -0500

    deleted a bunch of CordovaLib files

--- ota-app/platforms/ios/OTAApplication/Classes/NSFileManager+DoNotBackup.m
+++ ota-app/platforms/ios/OTAApplication/Classes/NSFileManager+DoNotBackup.m
@@ -13,19 +13,9 @@
 
 - (BOOL)addSkipBackupAttributeToItemAtURL:(NSURL *)URL
 {
-    if (&NSURLIsExcludedFromBackupKey == nil) { // iOS <= 5.0.1
-        const char* filePath = [[URL path] fileSystemRepresentation];
-        
-        const char* attrName = "com.apple.MobileBackup";
-        u_int8_t attrValue = 1;
-        
-        int result = setxattr(filePath, attrName, &attrValue, sizeof(attrValue), 0, 0);
-        return result == 0;
-    } else { // iOS >= 5.1
-        NSError *error = nil;
-        [URL setResourceValue:[NSNumber numberWithBool:YES] forKey:NSURLIsExcludedFromBackupKey error:&error];
-        return error == nil;
-    }
+    NSError *error = nil;
+    [URL setResourceValue:[NSNumber numberWithBool:YES] forKey:NSURLIsExcludedFromBackupKey error:&error];
+    return error == nil;
 }
 
 @end
